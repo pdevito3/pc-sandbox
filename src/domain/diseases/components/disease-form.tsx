1387ad12d4cfee3662b6c9371597bc9617d1de17
Paul DeVito <pdevito3@gmail.com>
2024-09-20 12:43:19 -0400

    width

--- src/domain/diseases/components/disease-form.tsx
+++ src/domain/diseases/components/disease-form.tsx
@@ -45,7 +45,7 @@ export function DiseaseForm({
   }, [isValid]);
 
   return (
-    <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-lg shadow-md dark:bg-gray-800">
+    <div className="max-w-xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md dark:bg-gray-800">
       <h2 className="text-2xl font-bold text-center text-gray-800 mb-6 dark:text-gray-200">
         {isEditMode ? "Edit Disease" : "Add Disease"}
       </h2>
